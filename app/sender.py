df15f320a6c4da074a6e1f4b305d9309178cd2d8
Paulo Calaes <paulocalaes@gmail.com>
2018-06-20 17:02:27 -0300

    Workers

--- app/sender.py
+++ app/sender.py
@@ -3,14 +3,16 @@ import redis
 import json
 from bottle import Bottle, request
 
+
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
-        self.route('/', callback=self.send)
+        self.route('/', method='POST', callback=self.send)
         self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
+
         DSN = 'dbname=email_sender user=postgres host=db'
         self.conn = psycopg2.connect(DSN)
-
+        
     def register_message(self, assunto, mensagem):
         SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
         cur = self.conn.cursor()
@@ -20,15 +22,14 @@ class Sender(Bottle):
 
         msg = {'assunto': assunto, 'mensagem': mensagem}
         self.fila.rpush('sender', json.dumps(msg))
-
-        print('Mensagem registrada!')
+        print('Mensagem registrada !')
 
     def send(self):
         assunto = request.forms.get('assunto')
         mensagem = request.forms.get('mensagem')
 
         self.register_message(assunto, mensagem)
-        return 'Mensagem enfileirada! Assunto: {} Mensagem: {}'.format(
+        return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
             assunto, mensagem
         )
 
